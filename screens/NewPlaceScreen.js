aa468c4cc206a9ebc72dcba19af890fa258d8077
olympia <owojcik7@gmail.com>
2020-01-22 11:06:41 -0500

    Get Started with Form in NewPlaceScreen and install redux

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,11 +1,28 @@
-import React from "react"
-import { View, Text, StyleSheet } from "react-native"
+import React, { useState } from "react"
+import { View, Text, StyleSheet, TextInput, ScrollView, Button } from "react-native"
+import Colors from "../constants/Colors"
 
 const NewPlaceScreen = props => {
+    const [titleValue, setTitleValue] = useState('')
+
+    const titleChangeHandler = text => {
+        //could add valudation before settings
+        setTitleValue(text)
+    }
+
+    const savePlaceHandler = () => {
+        //save the place using redux
+    }
+
     return (
-        <View>
-            <Text>NewPlaceScreen</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={titleValue} />
+                <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler}
+                />
+            </View>
+        </ScrollView>
     )
 }
 
@@ -13,6 +30,22 @@ NewPlaceScreen.navigationOptions = {
     headerTitle: "Add Place"
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    form: {
+        margin: 30
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
+    }
+
+})
 
 export default NewPlaceScreen
