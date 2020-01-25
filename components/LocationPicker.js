025fb9121aabfc820a2e3317c3941e44a6ef371a
olympia <owojcik7@gmail.com>
2020-01-25 10:14:07 -0500

    Get the User Location using expo-location

---
+++ components/LocationPicker.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react"
+//Button allows us to start getting user location, text for default text
+import { View, Button, Text, ActivityIndicator, Alert, StyleSheet } from "react-native"
+import * as Location from 'expo-location'
+import * as Permissions from 'expo-permissions'
+
+import Colors from "../constants/Colors"
+
+const LocationPicker = props => {
+    const [isFetching, setIsFetching] = useState(false)
+    const [pickedLocation, setPickedLocation] = useState()
+
+    const verifyPermissions = async () => {
+        const result = await Permissions.askAsync(Permissions.LOCATION)
+        if (result.status !== 'granted') {
+            Alert.alert('Insufficient Permissions!', 'You need to grant location permissions to use this app',
+                [{ text: 'Okay' }]
+            )
+            return false;
+        }
+        return true;
+    }
+
+
+    const getLocationHandler = async () => {
+        //check permissions
+        const hasPermission = await verifyPermissions()
+        if (!hasPermission) {
+            return;
+        }
+        //uses location API to get user location, returns a promise that will resolve when we get location
+        try {
+            setIsFetching(true)
+            const location = await Location.getCurrentPositionAsync({ timeout: 5000 });
+            console.log(location)
+            setPickedLocation({
+                lat: location.coords.latitude,
+                lng: location.coords.longitude
+            })
+
+        } catch (error) {
+            Alert.alert('Could not fetch location!', 'Please try again later or pick a location on the map.', [{ text: "Okay!" }])
+        }
+        setIsFetching(false)
+
+    }
+
+    return (
+        <View style={styles.locationPicker}>
+            <View style={styles.mapPreview}>
+                {isFetching ? <ActivityIndicator size='large' color={Colors.primary} /> : <Text>No location chosen yet!</Text>}
+            </View>
+            <Button title="Get User Location" color={Colors.primary} onPress={getLocationHandler} />
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    locationPicker: {
+        marginBottom: 15
+    },
+    mapPreview: {
+        marginBottom: 10,
+        width: '100%',
+        height: 150,
+        borderColor: "#ccc",
+        borderWidth: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    }
+})
+
+export default LocationPicker
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -4,6 +4,7 @@ import Colors from "../constants/Colors"
 import { useDispatch } from "react-redux"
 import * as placesActions from "../store/places-actions"
 import ImagePicker from "../components/ImagePicker"
+import LocationPicker from "../components/LocationPicker"
 
 const NewPlaceScreen = props => {
     const [titleValue, setTitleValue] = useState('')
@@ -32,6 +33,7 @@ const NewPlaceScreen = props => {
                 <Text style={styles.label}>Title</Text>
                 <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={titleValue} />
                 <ImagePicker onImageTake={imageTakenHandler} />
+                <LocationPicker />
                 <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler}
                 />
             </View>
