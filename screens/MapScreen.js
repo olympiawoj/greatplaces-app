6abfccaf12aaeaa2fbf31a23ba0398b4ddddda0a
olympia <owojcik7@gmail.com>
2020-01-27 11:25:59 -0500

    Add a Marker to MapScreen based on selectedLocation

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import { View, Text, StyleSheet } from "react-native"
-import MapView from "react-native-maps"
+import MapView, { Marker } from "react-native-maps"
 
 const MapScreen = props => {
+    const [selectedLocation, setSelectedLocation] = useState()
 
     const mapRegion = {
         latitude: 37.78,
@@ -10,8 +11,29 @@ const MapScreen = props => {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
     }
+
+    const selectLocationHandler = event => {
+        console.log(event)
+        setSelectedLocation({
+            lat: event.nativeEvent.coordinate.latitude,
+            lng: event.nativeEvent.coordinate.longitude
+        })
+    }
+
+    let markerCoordinates;
+    if (selectedLocation) {
+        markerCoordinates = {
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng
+        }
+    }
+
     return (
-        <MapView style={styles.map} region={mapRegion} />
+        <MapView style={styles.map} region={mapRegion} onPress={selectLocationHandler} >
+
+            {markerCoordinates && <Marker title="Picked Location" coordinate={markerCoordinates} />}
+
+        </MapView>
     )
 }
 
