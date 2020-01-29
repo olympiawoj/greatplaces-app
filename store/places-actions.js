138d8aef81036c990684df9d2f5c77a2585ccc0b
olympia <owojcik7@gmail.com>
2020-01-29 09:44:06 -0500

    Store the Address in redux store and database

--- store/places-actions.js
+++ store/places-actions.js
@@ -3,10 +3,27 @@ import * as FileSystem from 'expo-file-system'
 export const ADD_PLACE = "ADD_PLACE"
 export const SET_PLACES = "SET_PLACES"
 import { insertPlace, fetchPlaces } from "../helpers/db"
+import ENV from "../env"
 
 //image is temp path to the image
-export const addPlace = (title, image) => {
+export const addPlace = (title, image, location) => {
+
     return async  dispatch => {
+        //fetch human readable address - sends a get request, therefore we await the res
+        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=${ENV.googleApiKey}`)
+        if (!response.ok) {
+            throw new Error('Something went wrong while fetching address')
+        }
+        //extracts body of response and converts to normal JS
+        const resData = await response.json()
+        if (!resData.results) {
+            throw new Error('Something went wrong in resData.results')
+        }
+
+        const address = resData.results[0].formatted_address
+
+        console.log('resData', resData)
+
         //split by slashes and it converts this string into an arr of string segments
         //pop gives you the last segment
         const fileName = image.split('/').pop()
@@ -16,11 +33,20 @@ export const addPlace = (title, image) => {
                 from: image,
                 to: newPath
             })
-            const dbResult = await insertPlace(title, newPath, 'dummy address', 15.6, 12.3)
+            const dbResult = await insertPlace(title, newPath, address, location.lat, location.lng)
             // console.log(dbResult)
             dispatch({
                 type: ADD_PLACE,
-                placeData: { id: dbResult.insertId, title: title, image: newPath }
+                placeData: {
+                    id: dbResult.insertId,
+                    title: title,
+                    image: newPath,
+                    address: address,
+                    coords: {
+                        lat: location.lat,
+                        lng: location.lng
+                    }
+                }
             })
         } catch (error) {
             console.log(error)
--- store/places-reducer.js
+++ store/places-reducer.js
@@ -9,14 +9,26 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case SET_PLACES:
             return {
-                places: action.places.map(pl => new Place(pl.id.toString(), pl.title, pl.imageUri))
+                places: action.places.map(pl =>
+                    new Place(
+                        pl.id.toString(),
+                        pl.title,
+                        pl.imageUri,
+                        pl.address,
+                        pl.lat,
+                        pl.lng))
             }
         case ADD_PLACE:
             //id is dummy id using new Date
             const newPlace = new Place(
                 action.placeData.id.toString(),
                 action.placeData.title,
-                action.placeData.image)
+                action.placeData.image,
+                action.placeData.address,
+                action.placeData.coords.lat,
+                action.placeData.coords.lng
+            )
+
             return {
                 ...state,
                 //concat takes array, adds new item, and returns a new array
