03b11c4b4cce25672823e35466ff2c6468cbc389
olympia <owojcik7@gmail.com>
2020-01-29 08:21:13 -0500

    Update the NewPlaceScreen when the location changes

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -13,11 +13,14 @@ const LocationPicker = props => {
 
     const mapPickedLocation = props.navigation.getParam('pickedLocation')
 
+    const { onLocationPicked } = props
+
     useEffect(() => {
         if (mapPickedLocation) {
             setPickedLocation(mapPickedLocation)
+            props.onLocationPicked(mapPickedLocation)
         }
-    }, [mapPickedLocation])
+    }, [mapPickedLocation, onLocationPicked])
 
     const verifyPermissions = async () => {
         const result = await Permissions.askAsync(Permissions.LOCATION)
@@ -43,6 +46,10 @@ const LocationPicker = props => {
             const location = await Location.getCurrentPositionAsync({ timeout: 5000 });
             console.log('coords', location.coords)
             setPickedLocation({
+                lat: location.coords.latitude,
+                lng: location.coords.longitude
+            })
+            props.onLocationPicked({
                 lat: location.coords.latitude,
                 lng: location.coords.longitude
             })
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useCallback } from "react"
 import { View, Text, StyleSheet, TextInput, ScrollView, Button } from "react-native"
 import Colors from "../constants/Colors"
 import { useDispatch } from "react-redux"
@@ -9,6 +9,7 @@ import LocationPicker from "../components/LocationPicker"
 const NewPlaceScreen = props => {
     const [titleValue, setTitleValue] = useState('')
     const [selectedImage, setSelectedImage] = useState()
+    const [selectedLocation, setSelectedLocation] = useState()
 
     const dispatch = useDispatch()
 
@@ -19,7 +20,7 @@ const NewPlaceScreen = props => {
 
     const savePlaceHandler = () => {
         //save the place using redux
-        dispatch(placesActions.addPlace(titleValue, selectedImage))
+        dispatch(placesActions.addPlace(titleValue, selectedImage, selectedLocation))
         props.navigation.goBack()
     }
 
@@ -27,13 +28,18 @@ const NewPlaceScreen = props => {
         setSelectedImage(imagePath)
     }
 
+    const locationPickedHandler = useCallback(location => {
+        console.log('locatino in locationPickedHandler', location)
+        setSelectedLocation(location)
+    }, [])
+
     return (
         <ScrollView>
             <View style={styles.form}>
                 <Text style={styles.label}>Title</Text>
                 <TextInput style={styles.textInput} onChangeText={titleChangeHandler} value={titleValue} />
                 <ImagePicker onImageTake={imageTakenHandler} />
-                <LocationPicker navigation={props.navigation} />
+                <LocationPicker navigation={props.navigation} onLocationPicked={locationPickedHandler} />
                 <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler}
                 />
             </View>
