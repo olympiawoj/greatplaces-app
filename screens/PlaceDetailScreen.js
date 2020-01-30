f7c1b6072a9f9f4a2c4ef16e86b01213e9361c7d
olympia <owojcik7@gmail.com>
2020-01-30 09:03:31 -0500

    Finish the MapScreen

--- screens/PlaceDetailScreen.js
+++ screens/PlaceDetailScreen.js
@@ -8,6 +8,12 @@ const PlaceDetailScreen = props => {
     const placeId = props.navigation.getParam('placeId')
     const selectedPlace = useSelector(state => state.places.places.find(place => place.id === placeId))
 
+    const selectedLocation = { lat: selectedPlace.lat, lng: selectedPlace.lng }
+
+    const showMapHandler = () => {
+        props.navigation.navigate('Map', { readonly: true, initialLocation: selectedLocation })
+    }
+
     return (
         <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
             <Image source={{ uri: selectedPlace.imageUri }} style={styles.image} />
@@ -16,8 +22,10 @@ const PlaceDetailScreen = props => {
                     <Text style={styles.address}>{selectedPlace.address}</Text>
                 </View>
                 <MapPreview
-                    location={{ lat: selectedPlace.lat, lng: selectedPlace.lng }}
-                    style={styles.mapPreview} />
+                    location={selectedLocation}
+                    style={styles.mapPreview}
+                    onPress={showMapHandler}
+                />
 
             </View>
         </ScrollView>
